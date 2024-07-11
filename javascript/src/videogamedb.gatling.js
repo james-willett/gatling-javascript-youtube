e7b471364272043b202cdc840b3ba3238d03957a
James Willett <info@james-willett.com>
2024-07-11 10:00:27 +0100

    Part 3: Get Single Game With Parameter

--- javascript/src/videogamedb.gatling.js
+++ javascript/src/videogamedb.gatling.js
@@ -1,4 +1,4 @@
-import { atOnceUsers, scenario, simulation, jmesPath } from "@gatling.io/core";
+import { atOnceUsers, scenario, simulation, jmesPath, pause } from "@gatling.io/core";
 import { http, status } from "@gatling.io/http";
 
 export default simulation((setUp) => {
@@ -9,12 +9,12 @@ export default simulation((setUp) => {
     .contentTypeHeader("application/json");
 
   // Scenario
-  const myScenario = scenario("My Scenario")
-  .exec(http("Get All Games").get("/videogame")
+  const myScenario = scenario("My Scenario").exec(
+    http("Get All Games").get("/videogame")
         .check(status().is(200))
-        .check(jmesPath("[0].id").saveAs("firstGameId"))
-    )
-  .exec(http("Get Single Game").get("/videogame/#{firstGameId}")
+        .check(jmesPath("[0].id").saveAs("firstGameId")),
+    pause(5),
+    http("Get Single Game").get("/videogame/#{firstGameId}")
         .check(status().is(200))
         .check(jmesPath("name").is("Resident Evil 4"))
     );
