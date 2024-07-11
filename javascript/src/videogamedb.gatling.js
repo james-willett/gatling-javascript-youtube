a69941f9efeec971fa882ecda2f57caed26d622f
James Willett <info@james-willett.com>
2024-07-11 09:58:19 +0100

    Part 4: CSV file

--- javascript/src/videogamedb.gatling.js
+++ javascript/src/videogamedb.gatling.js
@@ -1,4 +1,4 @@
-import { atOnceUsers, scenario, simulation, jmesPath } from "@gatling.io/core";
+import { atOnceUsers, scenario, simulation, jmesPath, csv, pause, feed } from "@gatling.io/core";
 import { http, status } from "@gatling.io/http";
 
 export default simulation((setUp) => {
@@ -8,15 +8,22 @@ export default simulation((setUp) => {
     .acceptHeader("application/json")
     .contentTypeHeader("application/json");
 
+  // Feeder for test data
+  const feeder = csv("videogames.csv").random();
+
   // Scenario
-  const myScenario = scenario("My Scenario")
-  .exec(http("Get All Games").get("/videogame")
+  const myScenario = scenario("My Scenario").exec(
+    http("Get All Games").get("/videogame")
         .check(status().is(200))
-        .check(jmesPath("[0].id").saveAs("firstGameId"))
-    )
-  .exec(http("Get Single Game").get("/videogame/#{firstGameId}")
+        .check(jmesPath("[0].id").saveAs("firstGameId")),
+    pause(5),
+    http("Get Single Game").get("/videogame/#{firstGameId}")
         .check(status().is(200))
-        .check(jmesPath("name").is("Resident Evil 4"))
+        .check(jmesPath("name").is("Resident Evil 4")),
+    pause(2),
+    feed(feeder),
+    http("Get Game: #{gameName}").get("/videogame/#{gameId}")
+        .check(jmesPath("name").isEL("#{gameName}"))
     );
 
   // setUp block
